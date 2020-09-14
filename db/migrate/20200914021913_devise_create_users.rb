fe9135607ca5d5340536e014bd539937653a5cf4
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-09-14 11:34:16 +0900

    migrate

--- db/migrate/20200914021913_devise_create_users.rb
+++ db/migrate/20200914021913_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :nickname,           null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.date    :birthday,           null: false
+      t.integer :gender_id,          null: false
 
       ## Recoverable
       t.string   :reset_password_token
