cefeff27ca2a2737de567b326949ed6e651f8165
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-09-14 12:04:07 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+
+  VALID_email_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i.freeze
+  VALID_password_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i.freeze
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, format: { with: VALID_email_REGEX }
+    validates :password, format: { with: VALID_password_REGEX }
+    validates :birthday
+    validates :gender_id
+  end
+
 end
