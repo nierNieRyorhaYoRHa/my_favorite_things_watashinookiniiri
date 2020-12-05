2b7a05231ae77b3abf3b58d454797a21576480fb
nierNieRyorhaYoRHa <t.ishida.tech@gmail.com>
2020-12-05 14:35:14 +0900

    commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,9 +10,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sns_info = User.from_omniauth(request.env["omniauth.auth"])
     @user = sns_info[:user]
 
-    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+    if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-    else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+    else
       @sns_id = sns_info[:sns].id
       render template: 'devise/registrations/new'
     end  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
     @things = user.things
   end
 
+
+  def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      sign_in(@user)
+      redirect_to root_path and return
+    else
+      render :edit and return 
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :nickname, :birthday)
+  end
+
 end
